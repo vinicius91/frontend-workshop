84fdb68ed4b3a72df486f7255c7a825ff254ad1b
Vinicius Rodrigues <cin_vrodrigues@uolinc.com>
2019-07-22 15:06:54 -0300

    Assets

--- webpack/Exercicios/AssetManagement/src/index.js
+++ webpack/Exercicios/AssetManagement/src/index.js
@@ -1,10 +1,17 @@
 import _ from "lodash";
+import "./style.css";
+import sponge from "./sponge.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  // Add the image to our existing div.
+  const SpongeImg = new Image();
+  SpongeImg.src = sponge;
+
+  element.appendChild(SpongeImg);
 
   return element;
 }
--- webpack/Exercicios/Output/src/index.js
+++ webpack/Exercicios/Output/src/index.js
@@ -1,8 +1,19 @@
+import _ from "lodash";
+import "./style.css";
+import sponge from "./sponge.png";
+
+import printMe from "./print.js";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
+  const btn = document.createElement("button");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
